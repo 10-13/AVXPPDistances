ca3571a8f659006d2391be769e8affc4f8fb05c7
10-13 <103519265+10-13@users.noreply.github.com>
2024-04-18 19:53:07 +0300

    Update array.h

--- utils/alignarray/array.h
+++ utils/alignarray/array.h
@@ -8,7 +8,7 @@ namespace tt {
     namespace collections {
         namespace generic {
             template<typename T, size_t size, size_t _align = alignof(T) * 8ULL>
-                requires (_align >= alignof(T) / 8 && _align / 8 <= size * sizeof(T))
+                requires (_align / 8 >= alignof(T) && _align / 8 <= size * sizeof(T))
             struct alignas(_align / 8) align_array : public std::array<T, size> {};
         }
     }
